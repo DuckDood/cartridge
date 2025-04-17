ac999169a1a70009a5d99c5098ed713a4b14819a
DuckDood <b.c.youtub@gmail.com>
2025-04-18 11:43:56 +1200

    Added some keywords and statements

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 // thanks stackoverflow
 char* readfile(FILE *f) {
@@ -30,12 +31,21 @@ char* readfile(FILE *f) {
 
   return buffer;
 }
+// stackoverflow clutch
+int count_newlines(std::string s) {
+  int count = 0;
+
+  for (int i = 0; i < s.size(); i++)
+    if (s[i] == '\n') count++;
+
+  return count;
+}
 
 
 int main(int argc, char* argv[]) {
 	long length;
 	FILE* config;
-	FILE* makefile = fopen("Makefile", "w"); // probably gonna make a pr to change to c++ files
+	FILE* makefile = fopen("Makefile", "w"); // probably gonna make a pr to change to c++ file handling
 	if(argv[1] != NULL)
 		config = fopen(argv[1], "rb");
 	else {
@@ -50,43 +60,112 @@ int main(int argc, char* argv[]) {
 	fclose(config);
 	
 	std::string buffer = buf; // strings are way easier tbh
-	std::string makefileStr = "";
+	std::string makefile_string = "";
 	std::string line;
-	int tempGet;
+	int temp_get;
 	std::istringstream buff(buffer);
 	std::string token;
 	std::string token2;
+	std::string temp = "";
+	std::string compiler;
 	bool running = true;
 	bool inMake;
 	int i = 0;
+	int newline_count = count_newlines(buffer);
+	std::string end_all = "all: ";
+	std::vector<std::string> names;
+	std::vector<std::string> names2;
+	std::string nextobj = "";
+	std::string token3 = "";
+	bool noall = false;
 	while(running) {
-	std::getline(buff, line);
+	
 		i++;
-		tempGet = line.find(':');
-		if(tempGet == 0) {
+		if(i >= newline_count) running = false;	
+		std::getline(buff, line);
+	
+		temp_get = line.find(':');
+		if(temp_get == 0) {
 			std::cout << "Line:"<< i << ", Syntax error: Use of Colon suspiciously (in first character of line)\n";
 			exit(1);
 		}
-		if(tempGet == std::string::npos && !inMake) {
-			token = line;
-			//std::cout << token;
+		if(temp_get == std::string::npos && !inMake) {
+			temp_get = line.find(' ');
+			token = line.substr(0, temp_get);
+			token2 = line.substr(temp_get+1, line.length());
+			if(token == "include") {
+				token3 = token2.substr(token2.rfind("."), token2.length());
+				temp = token2.substr(0, token2.rfind("."));
+				if(nextobj != "") {
+					temp = nextobj;
+				}
+				std::cout << temp << "\n";
+				if(nextobj == "") {
+					temp = "obj/" + temp + ".o: " + token2;
+				} else {
+					temp = "obj/" + temp + ": " + token2;
+				}
+				nextobj = "";
+				if(!noall) {
+					names.push_back(temp.substr(0, temp.length() - token2.length() -2));
+					noall = false;
+				}
+				compiler = token3 == ".c"? "${CC}": "${CXX}"; // should change to fallback on c, not other way around.
+				// tabs in front of compiler, not space
+ 				temp += "\n	" + compiler + " " + token2 + " -c -o obj/" + token2.substr(0, token2.rfind('.')) + ".o";
+			//	std::cout << temp;
+				makefile_string += temp + "\n\n";
+
+			} else
+			if(token == "nextobj") {
+				nextobj = line.substr(temp_get+1, line.length());
+			} else
+			if(token == "build") {
+				token2 = line.substr(temp_get+1, line.length());
+				temp = "";
+				for(int i = 0; i < names.size(); i++) {
+					temp += names.at(i) + " ";
+				}	
+
+				token3 = "build/"+token2 + ": " + temp;
+				if(!noall) {
+					names.push_back("build/"+token2);
+					noall = false;
+				}
+				token3 += "\n	g++ " + temp + " -o build/" + token2;
+				token3 += "\n\n";
+				std::cout << token3 << "\n";
+				makefile_string += token3;
+			} else
+			if(token == "noall") {
+				noall = true;
+			} else
+			if(token == "adddir") {
+				
+			}
 		} else {
-			if (!inMake) {
-				token = line.substr(0, tempGet);
-				token2 = line.substr(tempGet+1, line.length());
+			if(!inMake) {
+				token = line.substr(0, temp_get);
+				names.push_back(token);
+				token2 = line.substr(temp_get+1, line.length());
 				inMake = true;
-				makefileStr += line + '\n';
+				makefile_string += line + '\n';
 			} else if(line == "end") {
 				inMake = false;
-				running = false;
 			} else {
-				makefileStr += line + '\n';
+				makefile_string += line + '\n';
 			}
 		}
 	}
 
-	std::cout << makefileStr;
-	fwrite(makefileStr.c_str(), sizeof(char), makefileStr.length(), makefile);
+	//std::cout << makefile_string;
+	for(int i = 0; i < names.size(); i++) {
+		end_all += names.at(i) + " ";
+	}	
+	end_all += '\n';
+	makefile_string = end_all + makefile_string;
+	fwrite(makefile_string.c_str(), sizeof(char), makefile_string.length(), makefile);
+	fclose(makefile);
 
 
 	return 0;
